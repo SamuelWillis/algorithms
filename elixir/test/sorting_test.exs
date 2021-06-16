ef643c5838b18017143ae1ccae0713d682f40a2b
Samuel Willis <samuel.w.h.willis@gmail.com>
2021-06-16 15:34:54 -0600

    Add merge sort to Sorting Context

--- elixir/test/sorting_test.exs
+++ elixir/test/sorting_test.exs
@@ -9,4 +9,11 @@ defmodule ElixirImpl.SortingTest do
       assert result == [1, 2, 3]
     end
   end
+
+  describe "merge_sort/1" do
+    test "returns sorted list" do
+      result = Sorting.merge_sort([3, 2, 1])
+      assert result == [1, 2, 3]
+    end
+  end
 end
