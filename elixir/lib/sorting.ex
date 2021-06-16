c2002f5a866837b9bba3094c4b85404b1e883c5c
Samuel Willis <samuel.w.h.willis@gmail.com>
2021-06-16 15:34:54 -0600

    Add merge sort to Sorting Context

--- elixir/lib/sorting.ex
+++ elixir/lib/sorting.ex
@@ -1,8 +1,14 @@
 defmodule ElixirImpl.Sorting do
   alias ElixirImpl.Sorting.InsertionSort
+  alias ElixirImpl.Sorting.MergeSort
 
   @doc """
   Use insertion sort to sort the provided list
   """
   defdelegate insertion_sort(list), to: InsertionSort, as: :sort
+
+  @doc """
+  Use merge sort to sort the provided list
+  """
+  defdelegate merge_sort(list), to: MergeSort, as: :sort
 end
